82e2d598f0f7639d2e8a4079e578b8aa1374230f
abhi894 <abhishekm894@gmail.com>
2019-11-05 20:23:39 +0530

    logout

--- user/includes/header.php
+++ user/includes/header.php
@@ -4,6 +4,8 @@ require 'config/config.php';
 
 if (isset($_SESSION['username'])) {
 	$userLoggedIn= $_SESSION['username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM user WHERE id='$userLoggedIn'");
+	$user = mysqli_fetch_array($user_details_query);
 }
 else{
 	header("Location: login.php");
@@ -18,6 +20,7 @@ else{
  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
  	<script src="assets/javascript/bootstrap.js"></script>
  	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+ 	<script src="https://kit.fontawesome.com/859866b78b.js" crossorigin="anonymous"></script>
  	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
  </head>
  <body>
@@ -27,10 +30,12 @@ else{
  			<a href="index.php">The Servicer</a>
  		</div>
  		<nav>
- 		<a href="#">Dashbard</a>
- 		<a href="#">My Bookings</a>
- 		<a href="#">Profile</a>
+ 		<a href="#"><i class="fas fa-home"></i> Dashbard</a>&nbsp
+ 		<a href="#"><i class="far fa-object-ungroup"></i> My Bookings</a> &nbsp
+ 		<a href="#"><i class="fas fa-users-cog"></i>  Settings</a>&nbsp
+ 		<a href="logout.php"><i class="fas fa-sign-out-alt"></i> Sign out</a>&nbsp
  	</nav>
+ 
 
 
  	</div>
